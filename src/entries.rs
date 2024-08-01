c770297ea2c9ec2feb9570a73d61e5e5f1e3562b
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2024-08-01 17:52:24 -0300

    fix path filter condition for files and dirs

--- src/entries.rs
+++ src/entries.rs
@@ -58,8 +58,11 @@ fn entries(dir: PathBuf, rm: RecurseMode) -> Box<dyn Iterator<Item = PathBuf>> {
         (!name.starts_with('.')).then_some(())?; // exclude hidden files and directories.
 
         (is_match(name, RE_IN.get(), RE_EX.get()) // applied to both files and directories.
-            && is_match(path.parent()?.to_str().unwrap(), RE_DIN.get(), RE_DEX.get())
-            && is_match(ext, RE_EIN.get(), RE_EEX.get()))
+            && match path.is_dir() {
+                true => is_match(path.to_str().unwrap(), RE_DIN.get(), RE_DEX.get()),
+                false => is_match(path.parent()?.to_str().unwrap(), RE_DIN.get(), RE_DEX.get())
+                    && is_match(ext, RE_EIN.get(), RE_EEX.get()),
+            })
         .into()
     }
 
