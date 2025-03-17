8587b1b2fc0da2ef655204f077349033a4437f01
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2025-03-17 16:45:02 -0300

    list flag to skip calculating all dir sizes

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -5,6 +5,7 @@ use anyhow::Result;
 use clap::{Args, ValueEnum};
 use human_repr::HumanCount;
 use std::cmp::Ordering;
+use std::sync::OnceLock;
 use yansi::{Color, Paint};
 
 #[derive(Debug, Args)]
@@ -18,6 +19,9 @@ pub struct List {
     /// Show full file paths.
     #[arg(short = 'p', long)]
     paths: bool,
+    /// Do not calculate directory sizes.
+    #[arg(short = 'c', long)]
+    no_calc_dirs: bool,
 }
 
 #[derive(Debug, Copy, Clone, ValueEnum)]
@@ -33,9 +37,10 @@ pub enum By {
 #[derive(Debug)]
 pub struct Media {
     entry: Entry,
-    size: u64,
-    count: u32,
+    size_count: Option<(u64, u32)>,
 }
+
+static CALC_DIR_SIZES: OnceLock<bool> = OnceLock::new();
 
 impl Refine for List {
     type Media = Media;
@@ -50,12 +55,17 @@ impl Refine for List {
         if let By::Path = self.by {
             self.paths = true;
         }
+        CALC_DIR_SIZES.set(!self.no_calc_dirs).unwrap();
     }
 
     fn refine(&self, mut medias: Vec<Self::Media>) -> Result<()> {
         // step: sort the files by name, size, or path.
         let compare = match self.by {
-            By::Size => |m: &Media, n: &Media| m.size.cmp(&n.size),
+            By::Size => |m: &Media, n: &Media| {
+                m.size_count
+                    .map(|(s, _)| s)
+                    .cmp(&n.size_count.map(|(s, _)| s))
+            },
             By::Name => |m: &Media, n: &Media| m.entry.file_name().cmp(n.entry.file_name()),
             By::Path => |m: &Media, n: &Media| m.entry.cmp(&n.entry),
         };
@@ -67,13 +77,16 @@ impl Refine for List {
 
         // step: display the results.
         medias.iter().for_each(|m| {
-            let size = format!("{}", m.size.human_count_bytes());
+            let (size, count) = match m.size_count {
+                Some((s, c)) => (&*format!("{}", s.human_count_bytes()), &*format!("{c}")),
+                None => ("?", "?"),
+            };
             match self.paths {
                 true => print!("{size:>8} {}", m.entry.display_path()),
                 false => print!("{size:>8} {}", m.entry.display_filename()),
             };
             if m.entry.is_dir() {
-                print!(" {} files", m.count.paint(Color::Blue).linger());
+                print!(" {} files", count.paint(Color::Blue).linger());
             }
             println!("{}", "".resetting());
         });
@@ -83,10 +96,13 @@ impl Refine for List {
             println!();
         }
         let (mut size, mut count) = (0, 0);
-        medias.iter().for_each(|m| {
-            size += m.size;
-            count += m.count;
-        });
+        medias
+            .iter()
+            .filter_map(|m| m.size_count)
+            .for_each(|(s, c)| {
+                size += s;
+                count += c;
+            });
         println!(
             "listed entries: {}{}",
             medias.len(),
@@ -102,8 +118,9 @@ impl TryFrom<Entry> for Media {
     type Error = (anyhow::Error, Entry);
 
     fn try_from(entry: Entry) -> Result<Self, Self::Error> {
-        let (size, count) = match entry.is_dir() {
-            true => {
+        let size_count = match (entry.is_dir(), CALC_DIR_SIZES.get().unwrap()) {
+            (true, false) => None,
+            (true, true) => {
                 let entries = Entries::single(&entry, Depth::Unlimited);
                 let mut count = 0;
                 let sum = entries
@@ -113,16 +130,16 @@ impl TryFrom<Entry> for Media {
                         e.metadata().map_or(0, |md| md.len())
                     })
                     .sum::<u64>();
-                (sum, count)
+                Some((sum, count))
             }
-            false => (
-                entry
+            (false, _) => {
+                let size = entry
                     .metadata()
                     .map_err(|err| (err.into(), entry.clone()))?
-                    .len(),
-                1,
-            ),
+                    .len();
+                Some((size, 1))
+            }
         };
-        Ok(Self { entry, size, count })
+        Ok(Self { entry, size_count })
     }
 }
