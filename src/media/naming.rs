c840b3acce15feb7787cd039087bd53376112c3e
Rogério Sampaio de Almeida <rsalmei@gmail.com>
2025-03-24 17:05:07 -0300

    improve maintainability of naming rules tests

--- src/media/naming.rs
+++ src/media/naming.rs
@@ -121,7 +121,9 @@ mod tests {
     #[test]
     fn strip_rules() {
         #[track_caller]
-        fn case(strip_rules: [&[&str]; 3], stem: &str, new_name: &str) {
+        fn case(rule: &[&str], idx: usize, stem: &str, new_name: &str) {
+            let mut strip_rules = [[""].as_ref(); 3];
+            strip_rules[idx] = rule;
             let mut medias = vec![Media(stem.to_owned())];
             let rules = Rules::compile(strip_rules, NO_REPLACE).unwrap();
             let warnings = rules.apply(&mut medias);
@@ -129,46 +131,46 @@ mod tests {
             assert_eq!(medias[0].0, new_name);
         }
 
-        case([&["Before"], &[], &[]], "beforefoo", "foo");
-        case([&["Before"], &[], &[]], "before foo", "foo");
-        case([&["Before"], &[], &[]], "Before__foo", "foo");
-        case([&["before"], &[], &[]], "Before - foo", "foo");
-        case([&["before"], &[], &[]], "before.foo", "foo");
-        case([&["before"], &[], &[]], "Before\t.  foo", "foo");
+        case(&["Before"], 0, "beforefoo", "foo");
+        case(&["Before"], 0, "before foo", "foo");
+        case(&["Before"], 0, "Before__foo", "foo");
+        case(&["before"], 0, "Before - foo", "foo");
+        case(&["before"], 0, "before.foo", "foo");
+        case(&["before"], 0, "Before\t.  foo", "foo");
 
-        case([&[], &["After"], &[]], "fooafter", "foo");
-        case([&[], &["After"], &[]], "foo after", "foo");
-        case([&[], &["After"], &[]], "foo__After", "foo");
-        case([&[], &["after"], &[]], "foo - After", "foo");
-        case([&[], &["after"], &[]], "foo.after", "foo");
-        case([&[], &["after"], &[]], "foo\t. After", "foo");
+        case(&["After"], 1, "fooafter", "foo");
+        case(&["After"], 1, "foo after", "foo");
+        case(&["After"], 1, "foo__After", "foo");
+        case(&["after"], 1, "foo - After", "foo");
+        case(&["after"], 1, "foo.after", "foo");
+        case(&["after"], 1, "foo\t. After", "foo");
 
         // exact: {BOUND}+{rule}$
-        case([&[], &[], &["Exact"]], "foo exact", "foo");
-        case([&[], &[], &["Exact"]], "foo__Exact", "foo");
-        case([&[], &[], &["exact"]], "foo - Exact", "foo");
-        case([&[], &[], &["exact"]], "foo.exact", "foo");
-        case([&[], &[], &["exact"]], "foo\t. Exact", "foo");
+        case(&["Exact"], 2, "foo exact", "foo");
+        case(&["Exact"], 2, "foo__Exact", "foo");
+        case(&["exact"], 2, "foo - Exact", "foo");
+        case(&["exact"], 2, "foo.exact", "foo");
+        case(&["exact"], 2, "foo\t. Exact", "foo");
 
         // exact: ^{rule}{BOUND}+
-        case([&[], &[], &["Exact"]], "exact foo", "foo");
-        case([&[], &[], &["Exact"]], "Exact__foo", "foo");
-        case([&[], &[], &["exact"]], "Exact - foo", "foo");
-        case([&[], &[], &["exact"]], "exact.foo", "foo");
-        case([&[], &[], &["exact"]], "Exact\t.  foo", "foo");
+        case(&["Exact"], 2, "exact foo", "foo");
+        case(&["Exact"], 2, "Exact__foo", "foo");
+        case(&["exact"], 2, "Exact - foo", "foo");
+        case(&["exact"], 2, "exact.foo", "foo");
+        case(&["exact"], 2, "Exact\t.  foo", "foo");
 
         // exact: {BOUND}+{rule}
-        case([&[], &[], &["Exact"]], "foo exact bar", "foo bar");
-        case([&[], &[], &["Exact"]], "foo__Exact-bar", "foo-bar");
-        case([&[], &[], &["exact"]], "foo - Exact_bar", "foo_bar");
-        case([&[], &[], &["exact"]], "foo.exact.bar", "foo.bar");
-        case([&[], &[], &["exact"]], "foo\t.  Exact - bar", "foo - bar");
+        case(&["Exact"], 2, "foo exact bar", "foo bar");
+        case(&["Exact"], 2, "foo__Exact-bar", "foo-bar");
+        case(&["exact"], 2, "foo - Exact_bar", "foo_bar");
+        case(&["exact"], 2, "foo.exact.bar", "foo.bar");
+        case(&["exact"], 2, "foo\t.  Exact - bar", "foo - bar");
 
         // exact: {rule}
-        case([&[], &[], &["Exact"]], "fexactoo", "foo");
-        case([&[], &[], &["Exact"]], "fexactoExacto", "foo");
-        case([&[], &[], &["Exact"]], "fooExact bar", "foo bar");
-        case([&[], &[], &["exact"]], "Exactfoo bar", "foo bar");
+        case(&["Exact"], 2, "fexactoo", "foo");
+        case(&["Exact"], 2, "fexactoExacto", "foo");
+        case(&["Exact"], 2, "fooExact bar", "foo bar");
+        case(&["exact"], 2, "Exactfoo bar", "foo bar");
 
         // exact: unfortunate case, where I'd need lookahead to avoid it...
         // case([&[], &[], &["Exact"]], "foo Exactbar", "foo bar");
